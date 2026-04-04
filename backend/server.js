c0b6b65a31ef3e9247e4cea925d83ceffc93d588
suraf-21 <ambiresurafel@gmail.com>
2026-04-04 04:38:25 -0700

    fix: update CORS with correct vercel domain

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','https://sura-portfolio-apv8.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002','https://suraf.versel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
